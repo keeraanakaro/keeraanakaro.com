86a0590f1cdb0ecb02fff08d2e44b91e6e7c5349
Viraj Kadam <hivirajkadam@gmail.com>
2020-09-02 02:16:59 +0530

    'Viraj : pull'

---
+++ up.php
@@ -0,0 +1,43 @@
+<?php
+
+$string = '';
+
+if (isset($_POST['submit']) && $_POST['password'] == '1234'){
+	$string = exec("git pull");
+}else{
+	$string = "Wrong Password.";
+}
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link href="https://getbootstrap.com/docs/4.0/dist/css/bootstrap.min.css" rel="stylesheet">
+		<link href="https://getbootstrap.com/docs/4.0/examples/floating-labels/floating-labels.css" rel="stylesheet">
+	</head>
+
+	<body>
+	<form class="form-signin" action="up.php" method="post" >
+		<div class="text-center mb-5">
+		<h1 class="h2 mb-3 font-weight-normal">Virajify Technologies</h1>
+		<h6>Portal for secure file upload to server.</h6>
+		</div>
+
+		<p class="text-center" style="font-size:12px;color:#777;">Enter Todays Password to Upload Files</p>
+		<div class="form-label-group">
+		<input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" pattern="\d{4}" maxlength="4" autofocus required autocomplete="off" >
+		<label for="inputPassword">Password</label>
+		</div>
+
+		<input class="btn btn-lg btn-success btn-block" type="submit" name="submit" value="UPLOAD">
+		<p class="text-center" style="font-size:12px;color:#777;">All Activities on this Page are Tracked & Monitored</p>
+
+		<p class="mt-5 bt-5 text-muted text-center"><?=$string?></p>
+
+	</form>
+	</body>
+</html>
