c951b3b704e21ff95d754b8d267482217ba69c4d
Viraj Kadam <hivirajkadam@gmail.com>
2020-09-10 19:39:18 +0530

    'Viraj : pull'

---
+++ career.php
@@ -0,0 +1,43 @@
+<?php require_once('files/config.php');?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php require_once('files/metatags.php'); ?>
+
+	<?php require_once('files/assets_css.php'); ?>
+</head>
+
+<body>
+	<header id="home" class="hero-area-2">
+		<div class="overlay"></div>
+		
+		<?php require_once('files/navbar.php'); ?>
+		
+		<div class="container">
+			<div class="row space-100">
+				<div class="col-lg-12 col-md-12 col-xs-12">
+					<div class="contents text-center">
+						<h2 class="head-title text-center">
+							Contact Keeraana
+						</h2>
+						<p class="text-center">Mail us your resume at 
+							<a href="mailto:admin@keeraana.com?subject=Careers%20Options%20at%20Keeraana&body=Hello%20Team%2C%0D%0A" target="_blank" style="color:#fff;" data-toggle="tooltip" title="Apply Today!" >
+								<b>admin@keeraana.com </b>
+							</a>
+						</p>
+					</div>
+				</div>
+			</div> 
+		</div>
+	</header>
+
+	<?php require_once('files/testimonials.php'); ?>
+
+	<?php require_once('files/subscribe.php'); ?>
+
+	<?php require_once('files/footer.php'); ?>
+
+	<?php require_once('files/assets_js.php'); ?>
+	
+</body>
+</html>
