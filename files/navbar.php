ea3505a853f39a3932fed550c61dd417d1e82ca4
unknown <ravikumathakur1993@gmail.com>
2021-01-13 21:26:40 +0530

    'Ravi : Navbar and Buttons Changes'

--- files/navbar.php
+++ files/navbar.php
@@ -1,9 +1,15 @@
 <nav class="navbar navbar-expand-md bg-inverse fixed-top scrolling-navbar">
 	<div class="container">
-		<a href="<?=$url;?>" class="navbar-brand"><img src="<?=$url;?>img/logo.png" alt="Logo" style="width:80%;"></a>  
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-			<i class="lni-menu"></i>
-		</button>
+		<div class="row space-100">
+			<div class="col-lg-8 col-md-8 col-sm-8 col-8">
+				<a href="<?=$url;?>" class="navbar-brand"><img src="<?=$url;?>img/logo.png" alt="Logo" style="width:120%;"></a> 
+			</div> 
+			<div class="col-lg-4 col-md-4 col-sm-4 col-4" class="navTop">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+					<i class="lni-menu"></i>
+				</button>
+			</div>
+		</div>
 		<div class="collapse navbar-collapse" id="navbarCollapse">
 			<ul class="navbar-nav mr-auto w-100 justify-content-end">
 				<li class="nav-item">
