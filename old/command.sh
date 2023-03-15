3ae54143b87ed96ef88e97329331113e522dc328
Viraj Kadam <hivirajkadam@gmail.com>
2023-03-15 16:51:52 +0530

    push

---
+++ old/command.sh
@@ -0,0 +1,18 @@
+git init
+
+git config core.fileMode false
+git config core.autocrlf false
+
+git add .
+git commit -m "'$1'"
+
+git rm -r --cached .
+git add .
+git commit -m "'$1'"
+
+git pull origin master
+
+chmod -R 777 ../keeraanakaro.com/
+chown -R www-data:www-data ../keeraanakaro.com/
+
+git push
